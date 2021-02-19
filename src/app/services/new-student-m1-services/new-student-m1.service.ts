27f1940f2ef81202a3ee312030d046c5739f0982
worng1234 <himax34529@gmail.com>
2021-02-19 12:21:45 +0700

    fix file

--- src/app/services/new-student-m1-services/new-student-m1.service.ts
+++ src/app/services/new-student-m1-services/new-student-m1.service.ts
@@ -1,22 +1,33 @@
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NewStudentM1Component } from '../../components/new-student-m1/new-student-m1.component';
-//test
+
 @Injectable({
   providedIn: 'root'
 })
 export class NewStudentM1Service {
-  readonly myAppurl = 'https://localhost:44342/';
-  readonly myApiurl = 'api/Newstudentm1';
+  url: string = 'https://localhost:44342/api/Newstudentm1';
+  newStudentm1 : any;
+  currentStudentm1 : any = {};
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
 
-  constructor( private http: HttpClient) { }
+
+  constructor( private http: HttpClient, private router: Router) { }
+  addNewstudentm1(newstudentm1){
+    this.newStudentm1 = {
+      "id" : newstudentm1.id,
+      "prename" : newstudentm1.prename,
+      "name" : newstudentm1.name,
+      "surname" : newstudentm1.surname,
+      "sex" : newstudentm1.sex,
+      "pic" : newstudentm1.pic,
+      "id_number" : newstudentm1.id_number,
+      "birthday" : newstudentm1.birthday,
+      "religion" : newstudentm1.religion,
+    }
+  }
 
 
 }
