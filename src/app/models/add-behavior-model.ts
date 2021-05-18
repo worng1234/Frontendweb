a1630400202aaffc96729c22476af4f28682661e
worng1234 <himax34529@gmail.com>
2021-05-18 22:56:13 +0700

    fix file

---
+++ src/app/models/add-behavior-model.ts
@@ -0,0 +1,11 @@
+export class Addbehavior{
+  b_id: number;
+  id : number;
+  student_id: number;
+  behavior_date : number;
+  pos_score : string;
+  neg_score : string;
+  title : string;
+  behavior_history: string;
+  score : string;
+}
