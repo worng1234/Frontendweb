366bcb44ad41077e88ebdc295eaf4ccfda72a70f
worng1234 <himax34529@gmail.com>
2021-06-07 13:22:22 +0700

    fix file

--- src/app/components/add-student-core/add-student-core.component.ts
+++ src/app/components/add-student-core/add-student-core.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-student-core.component.css']
 })
 export class AddStudentCoreComponent implements OnInit {
-
+  step: any = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submit(){
+    this.step = this.step + 1;
+  }
+  back(){
+    this.step = this.step - 1;
+  }
 }
