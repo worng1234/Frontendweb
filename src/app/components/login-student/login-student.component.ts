664957e8208814297d6fe9305455cc7eac78f102
worng1234 <54342155+worng1234@users.noreply.github.com>
2021-08-30 12:14:56 +0700

    fix file

--- src/app/components/login-student/login-student.component.ts
+++ src/app/components/login-student/login-student.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-student',
@@ -7,18 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginStudentComponent implements OnInit {
 
+  form: FormGroup;
   public data = {
     user:null,
     password: null
   };
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email:'',
+      password:''
+    });
   }
 
   onSubmit(){
-    console.log(this.data);
+    // console.log(this.data);
+    const formData = this.form.getRawValue();
+    const data = {
+      username: formData.email,
+      password: formData.password,
+      grant_type: 'password',
+      client_id: 2,
+      client_secret: 'ZBNaOGONUHaaVSllaZNSZzr4U5RKycBSwMNcCyuh',
+      scope: '*'
+    };
+
+    this.http.post('http://localhost:8000/oauth/token', data).subscribe(
+      result => {
+        console.log('success');
+        console.log(result);
+      },
+      error => {
+        console.log('error');
+        console.log(error);
+      }
+    );
   }
 
 }
