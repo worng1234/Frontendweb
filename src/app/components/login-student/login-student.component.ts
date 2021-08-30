75678eb57b5e7359d794ffcfb6aaad6cf4b214b1
worng1234 <54342155+worng1234@users.noreply.github.com>
2021-08-30 15:28:48 +0700

    fix file

--- src/app/components/login-student/login-student.component.ts
+++ src/app/components/login-student/login-student.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-student',
@@ -15,13 +16,20 @@ export class LoginStudentComponent implements OnInit {
     password: null
   };
 
-  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
       email:'',
       password:''
     });
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    });
+    console.log(localStorage.getItem('token'));
+    this.http.get('http://localhost:8000/api/User', {headers}).subscribe(
+      result => console.log(result)
+    );
   }
 
   onSubmit(){
